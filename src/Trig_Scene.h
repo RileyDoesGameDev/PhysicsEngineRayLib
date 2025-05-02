740419242808bea8d48b377969e384c1a976a6df
RileyDoesGameDev <Rileyartstuff12@gmail.com>
2025-05-02 13:29:25 -0600

    submitt changes

---
+++ src/Trig_Scene.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "scene.h"
+class TrigScene : public Scene 
+{
+public:
+
+	TrigScene(const std::string& title, int width, int height, const Color& background = BLACK) : 
+		Scene(title, width, height,background)
+	{
+
+	}
+
+private:
+
+
+	// Inherited via Scene
+	void Initialize() override;
+
+	void Update() override;
+
+	void Draw() override;
+
+	void DrawGUI() override;
+
+};
